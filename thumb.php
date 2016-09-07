de5d3e03cce4d194807b5ddd7119493ed8e6b353
Ravi Thanki <ravithanki15@yahoo.com>
2016-09-07 18:05:41 +0530

    Add files via upload
    initial commit

---
+++ thumb.php
@@ -0,0 +1,25 @@
+
+<?php
+$ffmpeg = 'C:\xampp\ffmpeg.exe';  
+
+//video dir  
+$video  = 'Tukur.mp4';   
+echo $video ;
+//where to save the image  
+$image = 'Tukur.jpg';  
+
+//time to take screenshot at  
+$interval = 1;  
+
+//screenshot size  pn 
+$size = '500*315';  
+
+//ffmpeg command  
+//$cmd = "$ffmpeg -i ".$video "-deinterlace -an -ss".$interval". -f mjpeg -t 1 -r 1 -y    -s". $size." ". $image." 2>&1";  
+$cmd="$ffmpeg -i ".$video." -ss 00:00:14.435 -f image2 -vframes 1 ". $size." ".$image;
+echo $cmd;
+//$cmd = "$ffmpeg  -i $video -f mjpeg -vframes 1 -s 150x150 -an myimage.jpg";
+exec($cmd);
+//$return = '$cmd';  
+//print_r($outputData);
+?>
